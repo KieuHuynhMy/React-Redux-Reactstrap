be8de2a53f0e6b3bbd58e7841bcc9fb5426c9ac9
Kay <socialnetworksite15@gmail.com>
2019-08-10 10:30:55 +0700

    Updates

---
+++ precache-manifest.9329fcad4b2284468c40fe5c295fecb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c38d7c972452a0927e0bc0248342dc",
+    "url": "/Pure-React-Restaurant1/index.html"
+  },
+  {
+    "revision": "8f90219b70a40627a909",
+    "url": "/Pure-React-Restaurant1/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fbd27d0c44bb621ad5ad",
+    "url": "/Pure-React-Restaurant1/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8f90219b70a40627a909",
+    "url": "/Pure-React-Restaurant1/static/js/2.c1f9334e.chunk.js"
+  },
+  {
+    "revision": "fbd27d0c44bb621ad5ad",
+    "url": "/Pure-React-Restaurant1/static/js/main.3ee9a97f.chunk.js"
+  },
+  {
+    "revision": "d1f01ccf18c21b62f57a",
+    "url": "/Pure-React-Restaurant1/static/js/runtime~main.e4766bbe.js"
+  }
+]);
