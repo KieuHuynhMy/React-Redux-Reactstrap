d8d9191959ad177a4ced6cd83eb71b4d1f4c029d
Kay <socialnetworksite15@gmail.com>
2019-09-09 13:24:58 +0700

    Updates

---
+++ precache-manifest.619e4b3a78ab42821f3f64ba2b521d83.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34bab93edfda40118940f4ba5bdf86e",
+    "url": "/Pure-React-Restaurant1/index.html"
+  },
+  {
+    "revision": "d97f72122ee50fed04c1",
+    "url": "/Pure-React-Restaurant1/static/css/2.4ea7e41b.chunk.css"
+  },
+  {
+    "revision": "9c7a5cb22c22c86d3e8a",
+    "url": "/Pure-React-Restaurant1/static/css/main.583c84c0.chunk.css"
+  },
+  {
+    "revision": "d97f72122ee50fed04c1",
+    "url": "/Pure-React-Restaurant1/static/js/2.9b50ea40.chunk.js"
+  },
+  {
+    "revision": "9c7a5cb22c22c86d3e8a",
+    "url": "/Pure-React-Restaurant1/static/js/main.688d2de9.chunk.js"
+  },
+  {
+    "revision": "d1f01ccf18c21b62f57a",
+    "url": "/Pure-React-Restaurant1/static/js/runtime~main.e4766bbe.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Pure-React-Restaurant1/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Pure-React-Restaurant1/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Pure-React-Restaurant1/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Pure-React-Restaurant1/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Pure-React-Restaurant1/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
